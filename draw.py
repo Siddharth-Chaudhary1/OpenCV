17793be153d1a1058414f5d72119a464cbe19054
Siddharth-Chaudhary1 <chsiddharth074@gmail.com>
2025-02-08 16:29:39 +0530

    draw in openCV

--- draw.py
+++ draw.py
@@ -19,7 +19,7 @@ cv.circle(blank, (blank.shape[1]//2, blank.shape[1]//2), 40, (0, 0, 255), thickn
 
 # draw a line
 cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[1]//2), (255, 255, 255), thickness=3)
-cv.imshow('Line', blank)
+# cv.imshow('Line', blank)
 
 # write text on img
 cv.putText(blank, "Hello", (255, 255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), 2)
